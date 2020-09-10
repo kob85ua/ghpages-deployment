5395608524c3a30a8b541caabdd4d4f74e104782
M.Kobeliev <kob85ua@gmail.com>
2020-09-10 16:53:27 +0300

    Updates

---
+++ precache-manifest.e5cafe0dc065f12c59e462e8f1a233a5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87d518701632fe8d31fbf2a0477e4cc4",
+    "url": "/ghpages-deployment/goit-react-hw01/index.html"
+  },
+  {
+    "revision": "8c93af011d89fe4f73ea",
+    "url": "/ghpages-deployment/goit-react-hw01/static/js/2.14d8fef0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/ghpages-deployment/goit-react-hw01/static/js/2.14d8fef0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7ac7c76fb4df382fa694",
+    "url": "/ghpages-deployment/goit-react-hw01/static/js/main.59bfd9d7.chunk.js"
+  },
+  {
+    "revision": "1c3718866024f21069cc",
+    "url": "/ghpages-deployment/goit-react-hw01/static/js/runtime-main.1ca32508.js"
+  }
+]);
